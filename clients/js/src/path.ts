49b435abcb53aedab757f06e92c3cb547473312f
Blockiosaurus <blockiosaurus@gmail.com>
2024-11-20 10:02:18 -0500

    Fix lint warnings.

--- clients/js/src/path.ts
+++ clients/js/src/path.ts
@@ -7,15 +7,18 @@ enum CustomPath {
 export const Path = {
   ...InternalPath,
   ...CustomPath,
-};
+} as const;
 
+// eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Path = InternalPath | CustomPath;
 
 export function buildPath(features: Path[]) {
   let path = 0;
 
+  // eslint-disable-next-line no-restricted-syntax
   for (const feature of features) {
-    if (feature != Path.RerollMetadata) {
+    if (feature !== Path.RerollMetadata) {
+      // eslint-disable-next-line no-bitwise
       path |= 1 << feature;
     }
   }
